db1a104841fc23272334e291b9d3939592b45965
Noch4nce <m4rgera123@mail.ru>
2022-10-21 22:02:49 +0500

    habit vanilla

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./habit-vanilla/src/**/*.{html,js}"],
+  content: ["./habit-vanilla/src/**/*.{html,js,css}"],
   theme: {
     extend: {},
   },
